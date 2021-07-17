c424884d81c4db15485d558acf14f3d9410bebdd
Arthur Miller <arthur.miller@live.com>
2021-07-17 10:06:20 +0200

    Put back accidentally removed region from previous commit.

--- lisp/extras.el
+++ lisp/extras.el
@@ -9,6 +9,18 @@
                    (float-time (time-subtract after-init-time before-init-time))) gcs-done))
 
 ;;;###autoload
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
+;;;###autoload
 (defun only-current-buffer ()
   (interactive)
   (mapc 'kill-buffer (cdr (buffer-list (current-buffer)))))
