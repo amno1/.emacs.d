5e9aadb547181e971977b3807b43cae1b6ba2c46
arthur <arthur.miller@live.com>
2025-12-22 14:47:09 +0100

    Don't update VC packages from official repos.

--- custom.el
+++ custom.el
@@ -4,6 +4,33 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(academic-phrases ace-window activities ascii-table async bash-alias beacon
+                      buttercup checkdoc common-lisp-snippets company consult
+                      corfu crux diminish dired-auto-readme dired-hacks-utils
+                      dired-narrow dired-quick-sort dired-setup dired-subtree
+                      disable-mouse eask edebug-x el-search elisp-benchmarks
+                      elisp-ergo-mode elnode eros expand-region exwm feebleline
+                      flimenu flycheck flycheck-package gh gif-screencast gist
+                      git-gutter git-gutter-fringe git-link git-messenger
+                      github-clone github-search gnu-elpa-keyring-update
+                      google-c-style goto-last-change helm helm-c-yasnippet
+                      helm-comint helm-dash helm-descbinds helm-flyspell
+                      helm-fuzzier helm-make helm-navi helm-org helm-pages
+                      helm-projectile helm-rg helm-smex helm-xref help-extras
+                      helpful hnreader ht htmlize hydra igist imenu-anywhere
+                      import-js inf-elixir jq-mode keycast kv lusty-explorer
+                      macro-math macrostep magit magit-filenotify magit-gh-pulls
+                      markdown-mode marshal modern-cpp-font-lock nov ob-async
+                      openwith org-appear org-bullets org-contrib org-download
+                      org-projectile org-projectile-helm org-ref org-sidebar
+                      org-superstar org-view-mode origami outli package-lint
+                      page-break-lines paxedit peep-dired polymode posframe
+                      prettier-js pretty-symbols projectile pulseaudio-control
+                      quick-peek refine request rjsx-mode sentex slime
+                      slime-company solarized-theme super-save
+                      top-override-keymap web-mode window-layout winum
+                      with-simulated-input yasnippet yasnippet-snippets))
  '(package-vc-selected-packages
    '((dired-setup :vc-backend Git :url "https://github.com/amno1/dired-setup")
      (elisp-ergo-mode :vc-backend Git :url
