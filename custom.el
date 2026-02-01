b8e90604adcd8ab1070956d4280543bd98a6c426
arthur <arthur.miller@live.com>
2026-02-01 14:32:14 +0100

    Don't use home-made macro-p predicate

--- custom.el
+++ custom.el
@@ -4,11 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dired-setup edebug-x elisp-extras help-extras peep-dired))
- '(package-vc-selected-packages
-   '((dired-setup :vc-backend Git :url "https://github.com/amno1/dired-setup")
-     (elisp-extras :vc-backend Git :url "https://github.com/amno1/elisp-extras")
-     (help-extras :vc-backend Git :url "https://github.com/amno1/help-extras"))))
+ '(package-selected-packages '(dired-setup edebug-x elisp-extras help-extras peep-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
