cbe2b3ac4ea305dc98ab7387dfadc7ab9fd41bcf
arthur <arthur.miller@live.com>
2026-02-01 15:32:01 +0100

    Refactor cl-setup in a separate package

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dired-setup edebug-x elisp-extras help-extras peep-dired)))
+ '(package-selected-packages
+   '(cl-setup dired-setup edebug-x elisp-extras help-extras peep-dired))
+ '(package-vc-selected-packages
+   '((cl-setup :vc-backend Git :url "https://github.com/amno1/cl-setup"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
